35bcdc2ea7841c4a2ed4b7f282b188008bd137ea
wakanore <138709595+wakanore@users.noreply.github.com>
2025-03-04 18:36:40 +0300

    hw-2 commit

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -6,7 +6,91 @@ using System.Threading.Tasks;
 
 namespace Application
 {
-    public class Class1
+    public class AuthorDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class AuthorService
+    {
+        private readonly Infrastructure.IAuthorRepository _AuthorRepository;
+
+        public AuthorService(Infrastructure.IAuthorRepository AuthorRepository)
+        {
+            _AuthorRepository = AuthorRepository;
+        }
+
+
+        public void AddAuthor(AuthorDto AuthorDto)
+        {
+            var Author = new Domain.Author
+            {
+                Name = AuthorDto.Name,
+                Phone = AuthorDto.Phone
+            };
+            _AuthorRepository.Add(Author);
+        }
+    }
+
+
+    public class ReaderDto
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class ReaderService
+    {
+        private readonly Infrastructure.IReaderRepository _ReaderRepository;
+
+        public ReaderService(Infrastructure.IReaderRepository ReaderRepository)
+        {
+            _ReaderRepository = ReaderRepository;
+        }
+
+
+        public void AddReader(ReaderDto ReaderDto)
+        {
+            var Reader = new Domain.Reader
+            {
+                Name = ReaderDto.Name,
+                Phone = ReaderDto.Phone
+            };
+            _ReaderRepository.Add(Reader);
+        }
+    }
+
+
+
+
+
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductService
+    {
+        private readonly Infrastructure.IProductRepository _ProductRepository;
+
+        public ProductService(Infrastructure.IProductRepository ProductRepository)
+        {
+            _ProductRepository = ProductRepository;
+        }
+
+
+        public void AddProduct(ProductDto ProductDto)
+        {
+            var Product = new Domain.Product
+            {
+                Name = ProductDto.Name
+            };
+            _ProductRepository.Add(Product);
+        }
     }
 }
--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Class1
+    public class Author
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+    }
+
+
+    public class Reader
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+    }
+
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Style { get; set; }
+        public string Id_author { get; set; }
+        public DateTime Date { get; set; }
+        public List<Reader> Readers { get; set; }
     }
 }
